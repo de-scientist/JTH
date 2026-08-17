ee3d08b0c2438d03c6a1e4397dfd34b05dd5157f
Mark Kinyanjui <gitaumark502@gmail.com>
2026-08-17 18:25:12 +0300

    update the savanna grilll image in the trusted by section

--- components/home/trusted-by-section.tsx
+++ components/home/trusted-by-section.tsx
@@ -7,7 +7,7 @@ import { fadeUp, staggerContainer, defaultTransition, viewportOnce } from '@/lib
 
 const clients = [
   { name: 'NovaTech Global Innovation', logo: '/images/novatech.jpg' },
-  { name: 'Savanna Grill', logo: '/images/bank3.png' },
+  { name: 'Savanna Grill', logo: '/images/grill.jpg' },
   { name: 'Wambui Fashion', logo: '/images/bank4.png' },
   { name: 'Elite Events', logo: '/images/G-F.png' },
   { name: 'Grace Community', logo: '/images/bank7.1.png' },
